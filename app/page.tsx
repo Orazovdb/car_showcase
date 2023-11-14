4821cd46c35d205b6d431da41d99e2015f1e1e4f
Dovran Orazov <dovranorazov@Dovran-Orazov.local>
2023-11-14 18:25:54 +0500

    Dovran text added

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default async function Home({ searchParams }: HomeProps) {
 
         <div className="mt-12 padding-x padding-y max-width" id="discover">
           <div className="home__text-container">
-            <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+            <h1 className="text-4xl font-extrabold">Car Catalogue Dovran</h1>
             <p>Explore out cars you might like</p>
           </div>
 
